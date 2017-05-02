360783eb1f75f9818324dac09f2cc9ffb9616c86
airobert <ai.robert.wangshuai@gmail.com>
2017-05-02 14:39:04 +0200

    start playing with plotting again

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+import os, sys, re
+
+from math import *
+
+
+
+def main():
+	pass
+	# Any code you like
+	plt.plot([1,2,3,4])
+	plt.ylabel('some numbers')
+	plt.show()
+
+
+if __name__ == '__main__':
+  main()
